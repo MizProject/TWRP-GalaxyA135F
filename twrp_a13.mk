9c1d078f582c207d36384905e1efbf561d0604d1
Mizumo-prjkt <bluepearl394@gmail.com>
2025-03-08 05:10:50 +0000

    Add initial device tree for Samsung SM-A135F (a13)

---
+++ twrp_a13.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Kernel is ARM64, core should then be 64 Bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from a13 device
+$(call inherit-product, device/samsung/a13/device.mk)
+
+PRODUCT_DEVICE := a13
+PRODUCT_NAME := twrp_a13
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A135F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a13nsxx-user 14 UP1A.231005.007 A135FXXU7EXF1 release-keys"
+
+BUILD_FINGERPRINT := samsung/a13nsxx/a13:14/UP1A.231005.007/A135FXXU7EXF1:user/release-keys
